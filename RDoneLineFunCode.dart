c33bbdb779543b4dc6265a7d8675d16e6af51aba
vBelyi <98315746+vBelyi@users.noreply.github.com>
2024-10-19 22:00:37 +0300

    в одну строку
    Просто переніс все в одну строку, він працює як однострочний але в гітхаб завантажився не одною строкою, тому змінив))

--- RDoneLineFunCode.dart
+++ RDoneLineFunCode.dart
@@ -3,24 +3,4 @@
 Напишіть код, що генерує абсолютно випадкове число від 1 до 100… але без генератора випадкових чисел. Ви ж таке можете, чи не так?
 */
 
-import 'dart:io';
-
-void main() => print(([
-      for (var FutureAsync = ([
-        "I",
-        "am",
-        "the",
-        "best",
-        "dart",
-        "developer",
-        "!!!!!",
-        1
-      ].reversed.toList()[0] as int);
-          FutureAsync <= int.parse("1100100", radix: 2);
-          FutureAsync += "i".length)
-        FutureAsync
-    ][(ProcessInfo.currentRss +
-            ((DateTime.now().second % 2 == 0)
-                ? DateTime.now().millisecondsSinceEpoch
-                : DateTime.now().millisecond)) %
-        100]));
+import 'dart:io';void main() => print(([for (var FutureAsync = (["I","am","the","best","dart","developer","!!!!!",1].reversed.toList()[0] as int);FutureAsync <= int.parse("1100100", radix: 2);FutureAsync += "i".length)FutureAsync][(ProcessInfo.currentRss +((DateTime.now().second % 2 == 0)? DateTime.now().millisecondsSinceEpoch: DateTime.now().millisecond)) %100]));
